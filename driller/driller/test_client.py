ef41728cadcb29d7f2d2a5dd940fd2e18b2dc3fa
Andrew Rutherfoord <andrew.rutherfoord@live.com>
2024-06-03 19:24:48 +0200

    Converted test client to AIO Pika

--- driller/driller/test_client.py
+++ driller/driller/test_client.py
@@ -1,7 +1,9 @@
+import asyncio
 from dataclasses import asdict
 import json
 import logging
 import time
+from typing import MutableMapping
 import pika
 import uuid
 
@@ -10,6 +12,10 @@ from driller.settings.default import LOG_FORMAT, LOG_LEVEL
 from driller.util import load_yaml, parse_config
 from .driller_config import DrillConfig, apply_defaults
 
+from aio_pika import Message, connect
+from aio_pika.abc import (
+    AbstractChannel, AbstractConnection, AbstractIncomingMessage, AbstractQueue,
+)
 
 logging.basicConfig(format=LOG_FORMAT, level=LOG_LEVEL)
 logger = logging.getLogger(__name__)
@@ -18,46 +24,87 @@ pika_logger.setLevel(logging.WARNING)
 
 class DrillerClient(object):
     queue = "driller_queue"
+    connection: AbstractConnection
+    channel: AbstractChannel
+    callback_queue: AbstractQueue
+    
+    def __init__(self) -> None:
+        self.futures: MutableMapping[str, asyncio.Future] = {}
+    
+    async def connect(self) -> "DrillerClient":
+        self.connection = await connect("amqp://guest:guest@localhost:5672/")
+        self.channel = await self.connection.channel()
+        self.callback_queue = await self.channel.declare_queue(exclusive=True)
+        await self.callback_queue.consume(self.on_response, no_ack=True)
 
-    def __init__(self):
-        self.connection = pika.BlockingConnection(
-            pika.ConnectionParameters(host="localhost")
-        )
+        return self
 
-        self.channel = self.connection.channel()
+    async def on_response(self, message: AbstractIncomingMessage) -> None:
+        if message.correlation_id is None:
+            print(f"Bad message {message!r}")
+            return
 
-        result = self.channel.queue_declare(queue="", exclusive=True)
-        self.callback_queue = result.method.queue
+        future: asyncio.Future = self.futures.pop(message.correlation_id)
+        future.set_result(message.body)
 
-        self.channel.basic_consume(
-            queue=self.callback_queue,
-            on_message_callback=self.on_response,
-            auto_ack=True,
-        )
+    async def call(self, task: DrillConfig) -> str:
+        correlation_id = str(uuid.uuid4())
+        loop = asyncio.get_running_loop()
+        future = loop.create_future()
 
-        self.response = None
-        self.corr_id = None
+        self.futures[correlation_id] = future
 
-    def on_response(self, ch, method, props, body):
-        if self.corr_id == props.correlation_id:
-            self.response = body
-
-    def call(self, task: DrillConfig):
-        task.neo = None
-        self.response = None
-        self.corr_id = str(uuid.uuid4())
-        self.channel.basic_publish(
-            exchange="",
-            routing_key="driller_queue",
-            properties=pika.BasicProperties(
-                reply_to=self.callback_queue,
-                correlation_id=self.corr_id,
+        await self.channel.default_exchange.publish(
+            Message(
+                task.to_json().encode(),
+                content_type="text/plain",
+                correlation_id=correlation_id,
+                reply_to=self.callback_queue.name,
             ),
-            body=task.to_json(),
+            routing_key=self.queue,
         )
-        while self.response is None:
-            self.connection.process_data_events(time_limit=None)
-        return self.response
+
+        return await future
+
+#     def __init__(self):
+#         self.connection = pika.BlockingConnection(
+#             pika.ConnectionParameters(host="localhost")
+#         )
+
+#         self.channel = self.connection.channel()
+
+#         result = self.channel.queue_declare(queue="", exclusive=True)
+#         self.callback_queue = result.method.queue
+
+#         self.channel.basic_consume(
+#             queue=self.callback_queue,
+#             on_message_callback=self.on_response,
+#             auto_ack=True,
+#         )
+
+#         self.response = None
+#         self.corr_id = None
+
+#     def on_response(self, ch, method, props, body):
+#         if self.corr_id == props.correlation_id:
+#             self.response = body
+
+#     def call(self, task: DrillConfig):
+#         task.neo = None
+#         self.response = None
+#         self.corr_id = str(uuid.uuid4())
+#         self.channel.basic_publish(
+#             exchange="",
+#             routing_key="driller_queue",
+#             properties=pika.BasicProperties(
+#                 reply_to=self.callback_queue,
+#                 correlation_id=self.corr_id,
+#             ),
+#             body=task.to_json(),
+#         )
+#         while self.response is None:
+#             self.connection.process_data_events(time_limit=None)
+#         return self.response
 
 def get_drill_configs(file) -> list[DrillConfig]:
     conf = load_yaml(file)
@@ -70,16 +117,16 @@ def get_drill_configs(file) -> list[DrillConfig]:
     
     return configs
 
-def main():
-    driller = DrillerClient()
+async def main():
+    driller = await DrillerClient().connect()
     configs = get_drill_configs("driller/configs/test.yaml")
 
     for conf in configs:
-        response = driller.call(conf)
+        response = await  driller.call(conf)
         logger.info(response)
 
 def exec():
-    main()
+    asyncio.run(main())
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
