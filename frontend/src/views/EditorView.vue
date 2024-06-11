14bfcaa462b4c65f80270695fcd18e0edb894db0
Andrew Rutherfoord <andrew.rutherfoord@live.com>
2024-06-11 23:02:15 +0200

    Added check config button to editor view

--- frontend/src/views/EditorView.vue
+++ frontend/src/views/EditorView.vue
@@ -3,6 +3,7 @@
     <v-app-bar>
       <v-app-bar-title>Editor</v-app-bar-title>
       <template v-slot:append>
+        <v-btn class="mx-2" variant="outlined" color="green" @click="checkConfig" prepend-icon="mdi-check">Check Config</v-btn>
         <v-btn variant="outlined" @click="executeDrillJob">Execute
           Drill Job</v-btn>
       </template>
@@ -34,8 +35,14 @@ const confirmLeaveMessage = "You have unsaved changes. Are you sure you want to 
 const content = ref("// Some code");
 const unsavedChanges = ref(false);
 
-async function executeDrillJob() {
-  let configuration = yaml.load(content.value)
+async function checkConfig(createValidNotification = true) {
+  let configuration;
+  try {
+    configuration = yaml.load(content.value)
+  } catch (e) {
+    toast.error("Could not parse yaml. Invalid syntax.")
+    return false
+  }
 
   const ajv = new Ajv();
   // Add formats extension to be able to check date formats
@@ -45,7 +52,12 @@ async function executeDrillJob() {
     // Check if input matches schema
     const isValid = ajv.validate(JSON.parse(schema), configuration)
 
-    if (!isValid) {
+    if (isValid) {
+      if (createValidNotification) {
+        toast.success("Configuration is valid!")
+      }
+      return true;
+    } else {
       let item = ajv.errors?.pop()
       let message = "";
       console.error(item)
@@ -56,20 +68,27 @@ async function executeDrillJob() {
         message = item?.message ? item.message : ":/";
       }
       toast.error("Configuration Invalid.", `Error at '${item?.instancePath}': ${message}\n`)
-      return
+      return false;
     }
   } catch (e) {
     toast.error("Could not parse configuration.")
-    console.error(e)
+    return false;
+  }
+
+}
+
+async function executeDrillJob() {
+  if (!checkConfig(false)) {
+    return
   }
 
   try {
-    let response = axios.post("http://127.0.0.1:8000/jobs/", configuration)
+    let response = axios.post("http://127.0.0.1:8000/jobs/", yaml.load(content.value))
     toast.success("Configuration Valid.")
   } catch (e) {
+    toast.error("Could not create jobs.")
     console.error(e)
   }
-  console.log(configuration)
 
 }
 
